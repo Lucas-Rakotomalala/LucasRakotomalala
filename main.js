a46ffb0f2908e8c7d52665608c7a345fe8671afc
Lucas Rakotomalala <lucas.rakotomalala@etu.univ-lyon1.fr>
2021-01-28 23:16:53 +0100

    Fin animation

--- main.js
+++ main.js
@@ -3,4 +3,35 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click', function () {
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 2000)
+    });
+
+    $('#up').on('click', function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 2000)
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    })
+
 });
+
+function temp() {
+    document.getElementById('form').action = "mailto:lucas.rakotomalala35@gmail.com?subject=Demande%20de%20services&body=Bonjour%20Monsieur%2C%20%0A%0AVoici%20ma%20demande%20de%20service%3A%0A%20" //+ document.getElementById('services').Value;
+}
+
+
+
